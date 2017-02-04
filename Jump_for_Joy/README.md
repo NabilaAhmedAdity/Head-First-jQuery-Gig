0103955ec9054acfe95d6dfb038fbddaf887ff7b
nabila <nabilaahmed.adity@gmail.com>
2017-02-04 22:42:23 +0600

    second version

--- Jump_for_Joy/README.md
+++ Jump_for_Joy/README.md
@@ -5,5 +5,8 @@ Here I have completed the following tasks:
 - The page has a header. 
 - The page has other four sections with a image per section. 
 - The sections are clickable. 
+- Highlight the box on which the mouse is placed.
 - When a user clicks on one of the sections, a message "Your Discount is" appears below the image of that section.
-- If a user clicks again, get rid of the last message and make a new one.
+- The doiscount should only be in one of the four boxes, the box can be changed after loading.
+- Visitors should only get one chance to find the discount when they load the page. So it will not be allowed to click more than once.
+- After thr visitor has made his guess and clicked on a abox, the answer should be reveled as to whether or not he got it right.
--- Jump_for_Joy/main.js
+++ Jump_for_Joy/main.js
@@ -1,9 +1,53 @@
 $(document).ready(function(){
 	//alert("Im am ready!");
-	$(".guess_box").click(function(){
-		$(".guess_box p").remove();
-		var discount = Math.floor((Math.random()*5) + 5);
-		var discount_msg = "<p> Your discount is " + discount + "% </p>";
-		$(this).append(discount_msg);
-	});
+
+    function getRandom(num){
+		var my_num = Math.floor(Math.random()*num);
+		return my_num;
+	}
+
+	var hideCode = function(){
+		var numRand = getRandom(4);
+		$(".guess_box").each(function(index, value){
+			if(numRand == index){
+				$(this).append("<span id='has_discount'></span>");
+				return false;
+			}
+		});
+	}
+
+	function checkForCode(){
+		var discount;
+		//contain: This method checks if whatever in the first parameter contains whatever in the second parameter.
+		//this: The current element, the one that called the function
+		if($.contains(this, document.getElementById("has_discount")))
+		{
+			var my_num = getRandom(100);
+			discount = "<p> Your discount is " + my_num + "% </p>";
+		}
+		else
+	    {
+	    	discount = "<p>Sorry, no discount this time!<p>";
+	    }
+	    $(this).append(discount);
+
+	    $(".guess_box").each(function(){
+	        $(this).unbind('click');
+	    });
+	}
+
+    hideCode();
+    //call the function when the element of .guess_box is clicked
+	$(".guess_box").click(checkForCode);
+
+	$(".guess_box").hover(
+
+		function(){
+			$(this).addClass("my_hover");
+		},
+		function(){
+			$(this).removeClass("my_hover");
+		}
+		);
+
 });
